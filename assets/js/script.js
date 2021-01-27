a58e103d2102c1181c3593c649e00271fe235b88
symlar <40181569+symlara@users.noreply.github.com>
2021-01-26 21:38:33 -0600

    added new function to script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ var weatherContainerEl = document.querySelector("#weather-container");
 var weatherSearchEl = document.querySelector("#citySearch");
 
 var getWeather = function(weather) {
-    var apiUrl = "api.openweathermap.org/data/2.5/weather?q=Columbia&state_code=38401&country_code=3166&temperature&appid=d69bf099c1a64c27c56575e55d93e3ef";
+    var apiUrl = "api.openweathermap.org/data/2.5/forecast?q=London,uk&callback=test&appid=d69bf099c1a64c27c56575e55d93e3ef";
+
 
     fetch(apiUrl)
     .then(function(reponse) {
@@ -15,3 +16,4 @@ var getWeather = function(weather) {
 var displayWeather = function(temperature) {
     
 }
+
