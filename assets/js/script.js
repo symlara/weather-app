b273c6415094e37d8a70941776cec88f47a8bf49
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-01-28 17:05:16 -0600

    added layout of html file. added first js piece to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,6 @@
-var weatherContainerEl = document.querySelector("#weather-container");
-var weatherSearchEl = document.querySelector("#citySearch");
+var form = document.querySelector(".search-form form");
 
-var getWeather = function(weather) {
-    var apiUrl = "api.openweathermap.org/data/2.5/forecast?q=London,uk&callback=test&appid=d69bf099c1a64c27c56575e55d93e3ef";
-
-
-    fetch(apiUrl)
-    .then(function(reponse) {
-        reponse.json().then(function(data) {
-            displayWeather(data);
-        })
-    });
-};
-
-var displayWeather = function(temperature) {
-    
-}
-
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const inputVal = input.value;
+});
