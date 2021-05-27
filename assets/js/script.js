58e97acb8335f123aa4346188dff72261b8f9d4a
symlara <40181569+symlara@users.noreply.github.com>
2021-05-27 15:42:03 -0500

    added displayWeather functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 let searchHistory = [];
 let lastCitySearched = "";
 
-let getCityWeater = function(city) {
+let getCityWeather = function(city) {
   let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ce39e7239416ad754359ca762d28521a&units=imperial";
 
   fetch(apiUrl)
@@ -23,4 +23,25 @@ let getCityWeater = function(city) {
   })
 
 };
+
+let searchSubmitHandler = function(e) {
+  e.preventDefault();
+
+  let cityName = $("#input").val().trim();
+
+  if(cityName) {
+    getCityWeather(cityName);
+
+    $("#input").val("");
+  }else {
+    alert('Please enter a city name here');
+  }
+};
+
+let displayWeather = function(weatherData) {
+  $("#city-name").text(weatherData.name + " (" + dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") + ") ").append(`<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
+  $("#city-temp").text("Temperature: " + weatherData.main.temp.toFixed(1) + "°F");
+  $("#city-humidity").text("Humidity: " + weatherData.main.humidity + "%");
+  $("#city-wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(1) + "mph");
+}
 
