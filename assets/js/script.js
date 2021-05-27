92900764eb5f8f92b3b6136d437bfb8cd001dfb0
symlara <40181569+symlara@users.noreply.github.com>
2021-05-27 15:33:43 -0500

    added fetch call to openweather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,24 @@ let lastCitySearched = "";
 
 let getCityWeater = function(city) {
   let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ce39e7239416ad754359ca762d28521a&units=imperial";
-  
-}
+
+  fetch(apiUrl)
+  .then(function(response) {
+    // response success
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayWeather(data);
+      });
+      // request failed
+    }else {
+      alert("Error: " + response.statusText);
+    }
+
+  })
+
+  .catch(function(error) {
+    alert("Unable to connect to OpenWeather App");
+  })
+
+};
+
